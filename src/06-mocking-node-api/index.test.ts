fa8709edc661f7ad95707c7dc95ea1464fdb8379
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-29 22:38:12 +0300

    feat: add test for interval and timeout

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,20 +1,8 @@
 // Uncomment the code below and write your tests
-import { readFileAsynchronously } from '.';
+import { doStuffByInterval, doStuffByTimeout, readFileAsynchronously } from '.';
 import path from 'path';
 import fs from 'fs';
 import fsPromises from 'fs/promises';
-
-jest.mock('fs', () => ({
-  existsSync: jest.fn(),
-}));
-
-jest.mock('fs/promises', () => ({
-  readFile: jest.fn(),
-}));
-
-jest.mock('path', () => ({
-  join: jest.fn(),
-}));
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
@@ -26,11 +14,26 @@ describe('doStuffByTimeout', () => {
   });
 
   test('should set timeout with provided callback and timeout', () => {
-    // Write your test here
+    const callback = jest.fn();
+    const timeout = 1000;
+    const setTimeoutSpy = jest.spyOn(global, 'setTimeout');
+    doStuffByTimeout(callback, timeout);
+
+    expect(setTimeoutSpy).toHaveBeenCalledWith(callback, timeout);
+    setTimeoutSpy.mockRestore();
   });
 
   test('should call callback only after timeout', () => {
-    // Write your test here
+    const callback = jest.fn();
+    const timeout = 1000;
+
+    doStuffByTimeout(callback, timeout);
+
+    expect(callback).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(timeout);
+
+    expect(callback).toHaveBeenCalled();
   });
 });
 
@@ -44,11 +47,31 @@ describe('doStuffByInterval', () => {
   });
 
   test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+    const callback = jest.fn();
+    const interval = 1000;
+
+    const setIntervalSpy = jest.spyOn(global, 'setInterval');
+
+    doStuffByInterval(callback, interval);
+
+    expect(setIntervalSpy).toHaveBeenCalledWith(callback, interval);
+
+    setIntervalSpy.mockRestore();
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
+    const callback = jest.fn();
+    const interval = 1000;
+    const numIntervals = 3;
+
+    doStuffByInterval(callback, interval);
+
+    expect(callback).not.toHaveBeenCalled();
+
+    for (let i = 0; i < numIntervals; i++) {
+      jest.advanceTimersByTime(interval);
+      expect(callback).toHaveBeenCalledTimes(i + 1);
+    }
   });
 });
 
