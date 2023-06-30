f1c54c3e0e95a1efbc253f5c77f2ff55a35de35c
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-30 11:22:17 +0300

    feat: add axios testing

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,62 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.runAllTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const axiosClient = {
+      get: jest.fn().mockResolvedValue({ data: '' }),
+    };
+
+    jest
+      .spyOn(axios, 'create')
+      .mockReturnValue(axiosClient as unknown as AxiosInstance);
+
+    const relativePath = '/posts';
+    await throttledGetDataFromApi(relativePath);
+
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const axiosClient = {
+      get: jest.fn().mockResolvedValue({ data: '' }),
+    };
+
+    jest
+      .spyOn(axios, 'create')
+      .mockReturnValue(axiosClient as unknown as AxiosInstance);
+
+    const relativePath = '/users';
+    await throttledGetDataFromApi(relativePath);
+
+    expect(axiosClient.get).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const responseData = { id: 1, title: 'Test Post' };
+    const axiosClient = {
+      get: jest.fn().mockResolvedValue({ data: responseData }),
+    };
+
+    jest
+      .spyOn(axios, 'create')
+      .mockReturnValue(axiosClient as unknown as AxiosInstance);
+
+    const result = await throttledGetDataFromApi('/posts');
+
+    expect(result).toEqual(responseData);
   });
 });
